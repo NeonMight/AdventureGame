a5bdbdfc66a535387c1d401d06b37a079967d1f2
justinmc995 <justin.mc995@gmail.com>
2014-11-10 08:23:09 -0600

    Added/altered some functions
    Made the player function use an index based system instead of accepting items as arguments, making it easier to use with doInput().
    Commented out old versions of functions and direction enumerator.

--- adventuregame.h
+++ adventuregame.h
@@ -2,19 +2,29 @@
 //Created by Stephen Hoffman, Ethan Waddle, & Justin McAlister
 #include <iostream>
 //Flow: Ask for user input, player does action, if monsters are present, they attack, repeat.
-enum Direction {NORTH, SOUTH, EAST, WEST};
+//enum Direction {NORTH, SOUTH, EAST, WEST};
 class Player //denotes the player character
 {
 	public:
 		Player(int health, int atk, string n, Room* l, Item** i, Weapon* currentwep);
 		~Player();
-		void go(); //go followed by north, south, east, or west will send player to adjacent room based on initialization list
+		void go(int); // Goes in a direction, 0-4, corresponding with n,s,e, and w
+		void get(int); //Takes int value to correspond with item index in room. When player gets a weapon, modify currentwep. Item is removed from room and added to inventory.
+		void drop(int); //Takes int value to correspond with item index in inventory to drop. Adds to room
+		void eat(int); //Adds food value, then consumes food
+		void attack(int); //does damage equal to player attack + currentwep. if null, + 0. Takes index of monster in room
+		/* void go(); //go followed by north, south, east, or west will send player to adjacent room based on initialization list
 		void get(Item i, Room r); //when player gets a weapon, modify currentwep; adds i from current room inventory to player inventory
 		void drop(Item i, Room r); //when player drops a weapon, modify currentwep
 		void eat(Food f); //adds food value, then deletes food
-		void attack(Monster m); //does damage equal to player attack + currentwep. if null, + 0
+		void attack(Monster m); //does damage equal to player attack + currentwep. if null, + 0 */
+		
 		void checkInventory(); //outputs all things in player inventory
-		void takeDamage(int d); //function to access player hp when monster attacks
+		//void takeDamage(int d); //function to access player hp when monster attacks
+		void modifyHealth(int); // Takes an int value and alters player's health by that value
+		void showHealth() const; // Prints out player's health
+		bool isAlive() const; // Returns status of the player, alive or dead
+		void doInput(string); // Function that takes user input
 	private:
 		int hp;		//hit points
 		int atk;	//attack value
@@ -29,7 +39,8 @@ class Room //each separate space. Player can travel between adjacent spaces with
 	public:
 		Room(int i, string n, Room** a, Item** t, Monster** m);
 		~Room();
-		void viewAdjacent() const; //prints out all possible directions to go from room
+		//void viewAdjacent() const; //prints out all possible directions to go from room
+		void searchRoom() const; // prints out all the items, monsters, and adjacent rooms
 	private:
 		int id;	//unique identifier
 		string name;	//basic name

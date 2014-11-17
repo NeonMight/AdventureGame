97b879050dc3529b3400026011ef0c688c780f0c
justinmc995 <justin.mc995@gmail.com>
2014-11-17 12:43:40 -0600

    Working on getting code to compile

--- adventuregame.cpp
+++ adventuregame.cpp
@@ -1,7 +1,9 @@
-#include "adventuregame.h"
+#include "player.h"
+#include "room.h"
+#include "item.h"
+#include "monster.h"
 #include <iostream>
-#include <string>
-#include <boost/algorithm/string.hpp>  
+#include <string> 
 
 ///////////
 ///Player
@@ -12,7 +14,7 @@ Player::Player(int health, int a, Room* l, Weapon* c)
 	hp = health;
 	atk = a;
 	location = l;
-	inventory = new item*[10];
+	inventory = new Item*[10];
 	currentwep = c;
 }
 
@@ -41,13 +43,14 @@ void Player::get(int x) {
 	location->give(x); // Take from room's inventory
 }
 
-void Player::eat(int x) {
-	/*Code to determine if item is food will go here*/
+//In progress
+/* void Player::eat(int x) {
+	//Code to determine if item is food will go here
 	int v = inventory[x]->getValue(); // Get healing value
 	modifyHealth(v); // Add to health
 	~*inventory[x];
 	inventory[x] = NULL; // Open space in inventory
-}
+}*/
 
 void Player::drop(int x) {
 	if (inventory[x] == NULL) {std::cout << "No item in this space.\n"; return;}
@@ -59,7 +62,7 @@ void Player::drop(int x) {
 }
 
 void Player::attack(int x) {
-	Monster* m = location->enemies[x];
+	Monster* m = location->monsterIndex(x);
 	if ( m == NULL ) { std::cout << "No enemy in this position.\n"; return; }
 	m->modifyHealth(-atk);
 	std::cout << "You attacked " << m->getName() << " for " << atk << " hit points.\n";
@@ -75,29 +78,32 @@ int Player::nextOpen() const {
 void Player::checkInventory() const {
 	std::cout << "Inventory:\n";
 	for (int i = 0; i < 10; i++) {
-		std::cout << i < ": ";
-		if (inventory[i] != NULL) ? std::cout << inventory[i]->getName() << "\n" : std::cout << "Empty\n";
+		std::cout << i << ": ";
+		if (inventory[i] != NULL) {std::cout << inventory[i]->getName() << "\n";}
+		else {std::cout << "Empty\n";}
 	}
 }
 
 void Player::modifyHealth(int x) {
-	player.hp += x;
+	hp += x;
 	std::cout << "You have ";
-	if (x > 0) ? std::cout << "gained ": std::cout << "lost ";
+	if (x > 0) {std::cout << "gained ";}
+	else {std::cout << "lost ";}
 	std::cout << x << " health.\n";
 }
 
 void Player::showHealth() const {
-	std::cout << "Your current health is " << player.hp << ".\n";
+	std::cout << "Your current health is " << hp << ".\n";
 }
 
 bool Player::isAlive() const {
-	if (player.hp > 0) ? return true : return false;
+	if (hp > 0) {return true;}
+	else {return false;}
 }
 
-void Player::doInput(string s) {
+void Player::doInput(std::string s) {
 	// Create 2 new arrays, each to hold a word.
-	string args[2];
+	std::string args[2];
 	
 	// Put first word into first array
 	int i = 0;
@@ -117,85 +123,74 @@ void Player::doInput(string s) {
 	
 	// Make the strings lowercase so they can be checked without being case sensitive.
 	for (int k = 0; k < 2; k++)
-		boost::algorithm::string::to_lower(args[k]); // Should convert string to lowercase. Have not tested it yet.
+		for (int l = 0; l != '\0'; l++ )
+			std::tolower(args[k][l]); // Should convert string to lowercase. Have not tested it yet.
 		
 	// Base input checker, takes the first word and uses the switch to call the needed function
-	switch (args[0]) {
-		case "eat": goto eat;
-		case "get": goto get;
-		case "drop": goto drop;
-		case "go": goto go;
-		case "attack": goto attack;
-		case "search": location->searchRoom(); return;
-		case "inventory": checkInventory(); return;
-		case "health": showHealth(); return;
-		case "help": goto help;
-		default: std::cout << "Unknown command, use 'help' to view commands.\n"; return;
-	}
+	if (args[0] == "eat") goto eat;
+	else if (args[0] == "get") goto get;
+	else if (args[0] == "drop") goto drop;
+	else if (args[0] == "go") goto go;
+	else if (args[0] == "attack") goto attack;
+	else if (args[0] == "search") {location->searchRoom(); return;}
+	else if (args[0] == "inventory") {checkInventory(); return;}
+	else if (args[0] == "health") {showHealth(); return;}
+	else if (args[0] == "help") goto help;
+	else {std::cout << "Unknown command, use 'help' to view commands.\n"; return;}
 	
 eat:
-	switch (args[1]) {
-		case "0": eat(0); return;
-		case "1": eat(1); return;
-		case "2": eat(2); return;
-		case "3": eat(3); return;
-		case "4": eat(4); return;
-		case "5": eat(5); return;
-		case "6": eat(6); return;
-		case "7": eat(7); return;
-		case "8": eat(8); return;
-		case "9": eat(9); return;
-		default: std::cout << "Invalid argument for 'eat'.\nValid arguments for 'eat' are numbers 0-9.\n"; return;
-	}
+	if (args[1] == "0") {eat(0); return;}
+	else if (args[1] == "1") {eat(1); return;}
+	else if (args[1] == "2") {eat(2); return;}
+	else if (args[1] == "3") {eat(3); return;}
+	else if (args[1] == "4") {eat(4); return;}
+	else if (args[1] == "5") {eat(5); return;}
+	else if (args[1] == "6") {eat(6); return;}
+	else if (args[1] == "7") {eat(7); return;}
+	else if (args[1] == "8") {eat(8); return;}
+	else if (args[1] == "9") {eat(9); return;}
+	else {std::cout << "Invalid argument for 'eat'.\nValid arguments for 'eat' are numbers 0-9.\n"; return;}
 	
 get:
-	switch (args[1]) {
-		case "0": get(0); return;
-		case "1": get(1); return;
-		case "2": get(2); return;
-		case "3": get(3); return;
-		case "4": get(4); return;
-		case "5": get(5); return;
-		case "6": get(6); return;
-		case "7": get(7); return;
-		case "8": get(8); return;
-		case "9": get(9); return;
-		default: std::cout << "Invalid argument for 'get'.\nValid arguments for 'get' are numbers 0-9.\n"; return;
-	}
+	if (args[1] == "0") {get(0); return;}
+	else if (args[1] == "1") {get(1); return;}
+	else if (args[1] == "2") {get(2); return;}
+	else if (args[1] == "3") {get(3); return;}
+	else if (args[1] == "4") {get(4); return;}
+	else if (args[1] == "5") {get(5); return;}
+	else if (args[1] == "6") {get(6); return;}
+	else if (args[1] == "7") {get(7); return;}
+	else if (args[1] == "8") {get(8); return;}
+	else if (args[1] == "9") {get(9); return;}
+	else {std::cout << "Invalid argument for 'get'.\nValid arguments for 'get' are numbers 0-9.\n"; return;}
 	
 drop:
-	switch (args[1]) {
-		case "0": drop(0); return;
-		case "1": drop(1); return;
-		case "2": drop(2); return;
-		case "3": drop(3); return;
-		case "4": drop(4); return;
-		case "5": drop(5); return;
-		case "6": drop(6); return;
-		case "7": drop(7); return;
-		case "8": drop(8); return;
-		case "9": drop(9); return;
-		default: std::cout << "Invalid argument for 'drop'.\nValid arguments for 'drop' are numbers 0-9.\n"; return;
-	}
+	if (args[1] == "0") {drop(0); return;}
+	else if (args[1] == "1") {drop(1); return;}
+	else if (args[1] == "2") {drop(2); return;}
+	else if (args[1] == "3") {drop(3); return;}
+	else if (args[1] == "4") {drop(4); return;}
+	else if (args[1] == "5") {drop(5); return;}
+	else if (args[1] == "6") {drop(6); return;}
+	else if (args[1] == "7") {drop(7); return;}
+	else if (args[1] == "8") {drop(8); return;}
+	else if (args[1] == "9") {drop(9); return;}
+	else {std::cout << "Invalid argument for 'drop'.\nValid arguments for 'drop' are numbers 0-9.\n"; return;}
 	
 go:
-	switch (args[1]) {
-		case "n": go(0); return;
-		case "s": go(1); return;
-		case "e": go(2); return;
-		case "w": go(3); return;
-		default: std::cout << "Invalid argument for 'go'.\nValid arguments for 'go' are 'n', 's', 'e', and 'w'.\n"; return;
-	}
+	if (args[1] == "n") {go(0); return;}
+	else if (args[1] == "s") {go(1); return;}
+	else if (args[1] == "e") {go(2); return;}
+	else if (args[1] == "w") {go(3); return;}
+	else {std::cout << "Invalid argument for 'go'.\nValid arguments for 'go' are 'n', 's', 'e', and 'w'.\n"; return;}
 	
 attack:
-	switch (args[1]) {
-		case "0": attack(0); return;
-		case "1": attack(1); return;
-		case "2": attack(2); return;
-		case "3": attack(3); return;
-		case "4": attack(4); return;
-		default: std::cout << "Invalid argument for 'attack'.\nValid arguments for 'attack' are numbers 0-4.\n"; return;
-	}
+	if (args[1] == "0") {attack(0); return;}
+	else if (args[1] == "1") {attack(1); return;}
+	else if (args[1] == "2") {attack(2); return;}
+	else if (args[1] == "3") {attack(3); return;}
+	else if (args[1] == "4") {attack(4); return;}
+	else {std::cout << "Invalid argument for 'attack'.\nValid arguments for 'attack' are numbers 0-4.\n"; return;}
 	
 help:
 	std::cout << "Valid commands are 'eat', 'get', 'drop', 'go', 'attack', 'search', 'inventory', 'health', and 'help'.\n";
@@ -206,10 +201,9 @@ help:
 ///Room
 ///////////
 
-Room::Room(string n, Room** a, Item** t, Monster** m)
-{
+Room::Room(std::string n, Room** a, Item** t, Monster** m) {
 	name = n;
-	adjacent = new Room*[4]; no more than 4 adjacent rooms
+	adjacent = new Room*[4]; //no more than 4 adjacent rooms
 	inventory = new Item*[10];	//no more than 10 items per room
 	enemies = new Monster*[5];	//no more than 5 enemies per room
 	//if there is an overflow, for loop will handle it
@@ -221,7 +215,7 @@ Room::Room(string n, Room** a, Item** t, Monster** m)
 	//construct inventory
 	for( int j = 0; j < 10; j++)
 	{
-		inventory[j] = i[j];
+		inventory[j] = t[j];
 	}
 	//construct enemies
 	for( int k = 0; k < 5; k++)
@@ -240,25 +234,34 @@ Room::~Room()
 void Room::searchRoom() const {
 	std::cout << "Items:\n";
 	for (int i = 0; i < 10; i++) {
-		std::cout << i < ": ";
-		if (inventory[i] != NULL) ? std::cout << inventory[i]->getName() << "\n" : std::cout << "Empty\n";
+		std::cout << i << ": ";
+		if (inventory[i] != NULL) std::cout << inventory[i]->getName() << "\n";
+		else std::cout << "Empty\n";
 	}
 	
 	std::cout << "Monsters:\n";
 	for (int i = 0; i < 5; i++) {
-		std::cout << i < ": ";
-		if (enemies[i] != NULL) ? std::cout << enemies[i]->getName() << "\n" : std::cout << "No Enemy\n";
+		std::cout << i << ": ";
+		if (enemies[i] != NULL) std::cout << enemies[i]->getName() << "\n";
+		else std::cout << "No Enemy\n";
 	}
 	
 	std::cout << "Connected Rooms:\n";
 	std::cout << "North: ";
-	if (adjacent[0] != NULL) ? std::cout << adjacent[0]->getName() << "\n" : std::cout << "No Passage\n";
+	if (adjacent[0] != NULL) std::cout << adjacent[0]->getName() << "\n";
+	else std::cout << "No Passage\n";
+	
 	std::cout << "South: ";
-	if (adjacent[1] != NULL) ? std::cout << adjacent[1]->getName() << "\n" : std::cout << "No Passage\n";
+	if (adjacent[1] != NULL) std::cout << adjacent[1]->getName() << "\n";
+	else std::cout << "No Passage\n";
+	
 	std::cout << "East: ";
-	if (adjacent[2] != NULL) ? std::cout << adjacent[2]->getName() << "\n" : std::cout << "No Passage\n";
+	if (adjacent[2] != NULL) std::cout << adjacent[2]->getName() << "\n";
+	else std::cout << "No Passage\n";
+	
 	std::cout << "West: ";
-	if (adjacent[3] != NULL) ? std::cout << adjacent[3]->getName() << "\n" : std::cout << "No Passage\n";
+	if (adjacent[3] != NULL) std::cout << adjacent[3]->getName() << "\n";
+	else std::cout << "No Passage\n";
 }
 
 void Room::give(int x) {
@@ -276,6 +279,14 @@ int Room::nextOpen() const {
 		if ( inventory[i] == NULL ) return i;
 	}
 	return -1; // No space available
+}
+
+Item* Room::atIndex(int x) const {
+	return inventory[x];
+}
+
+Monster* Room::monsterIndex(int x) const {
+	return enemies[x];
 }
 
 string Room::getName() const {

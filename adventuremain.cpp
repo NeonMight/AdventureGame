2ab9422e48ab2f6397fe30c893202956ec232d6c
justinmc995 <justin.mc995@gmail.com>
2014-11-18 15:03:05 -0600

    Update adventuremain.cpp

--- adventuremain.cpp
+++ adventuremain.cpp
@@ -2,7 +2,7 @@
 // Created by Stephen Hoffman, Ethan Waddle, & Justin McAlister
 
 #include "player.h"
-//#include "room.h"
+#include "room.h"
 #include <iostream>
 #include <string> 
 using namespace std;
@@ -11,11 +11,14 @@ int main() {
 	string input;
 	
 	//Initializations
-	Player p(100,10,NULL,NULL); // This is placeholder code.
+	/*Room r("Test",NULL,NULL,NULL);
+	Room* startroom = &r;*/
+	Player p(100,10,/*startroom*/NULL,NULL); // This is placeholder code.
 
 	// Game loop
 	// Flow: Ask for user input, player does action, if monsters are present, they attack, repeat.
 	while (p.isAlive()) { // Continue looping while the player is alive
+		cout << "Input Command:";
 		cin >> input;
 		p.doInput(input); // This function will convert user input into a function call. All game output will be from a function called from here.
 	}

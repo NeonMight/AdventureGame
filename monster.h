848e78bbfcd4b2f7d156d22a319757649330b5fc
justinmc995 <justin.mc995@gmail.com>
2014-11-19 17:01:21 -0600

    We probably don't need attack() for monster, a player function for monster attacks will be better

--- monster.h
+++ monster.h
@@ -8,10 +8,10 @@ class Monster //monsters will attack player if player is in same room. Player ca
 	public:
 		//Monster(int health, int a, string n, Room* l, Weapon* c); //will call player constructor with same parameters
 		Monster(int health, int a, std::string n); // Monster constructor
-		~Monster();
+		//~Monster();
 		std::string getName() const;
 		void modifyHealth(int);
-		void Attack(Player p); //monster will call modifyHealth on player and subtract attack value
+		void Attack(Player p); //monster will call modifyHealth on player and subtract attack value (This probably isn't necessary)
 	private:
 		int atk; // Attack damage
 		int hp; // Hit points

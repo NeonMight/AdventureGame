9b866773e099b78c238bc13f515877d513506f9f
justinmc995 <justin.mc995@gmail.com>
2014-11-19 12:01:13 -0600

    Tweak to get doInput() to work correctly

--- adventuremain.cpp
+++ adventuremain.cpp
@@ -19,7 +19,7 @@ int main() {
 	// Flow: Ask for user input, player does action, if monsters are present, they attack, repeat.
 	while (p.isAlive()) { // Continue looping while the player is alive
 		cout << "Input Command:";
-		cin >> input;
+		getline(cin,input);
 		p.doInput(input); // This function will convert user input into a function call. All game output will be from a function called from here.
 		cout << "\n";
 	}

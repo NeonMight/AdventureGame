8f4174543fea5c6039cb1ff3aaabb19eca936654
justinmc995 <justin.mc995@gmail.com>
2014-11-19 16:59:12 -0600

    Removed derived classes, only item is used

--- item.h
+++ item.h
@@ -6,32 +6,12 @@
 class Item //items are things that the player can add to inventory. Some points in game may check inventory to see if player has that item.
 {
 	public:
-		Item(std::string,int);
+		Item(std::string,int,int);
 		~Item();
 		std::string getName() const;
+		int getValue() const;
 	private:
 		std::string name;
-		int id;
-};
-
-//derived classes
-
-class Food : public Item //food will restore player health
-{
-	public:
-		Food(std::string n, int iid, int v); //calls item constructor
-		~Food();
-		int getValue() const;
-	private:
-		int val; //amount of hit points you will recover
-};
-
-class Weapon : public Item //temporarily boosts player attack. player cannot have more than one
-{
-	public:
-		Weapon(std::string n, int iid, int v); //calls item constructor
-		~Weapon();
-		int getValue() const;
-	private:
-		int val; //attack stat increase
+		int id; // 0 = None, 1 = Food, 2 = Weapon
+		int val; // Value for food or weapon
 };

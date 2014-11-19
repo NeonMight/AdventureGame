e7660e6601019b3798b58599f86ed7561ccb76ae
justinmc995 <justin.mc995@gmail.com>
2014-11-19 17:41:46 -0600

    Added getHealth() function

--- monster.h
+++ monster.h
@@ -11,6 +11,7 @@ class Monster //monsters will attack player if player is in same room. Player ca
 		//~Monster();
 		std::string getName() const;
 		void modifyHealth(int);
+		int getHealth() const;
 		//void Attack(Player p); //monster will call modifyHealth on player and subtract attack value (This probably isn't necessary)
 	private:
 		int atk; // Attack damage

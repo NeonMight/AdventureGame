ff6d2e8bcea4f717511aa0ed7856d5021b7c3e82
NeonMight <NeonMight@users.noreply.github.com>
2014-11-19 15:26:28 -0600

    Added some test code
    Added a few lines of code testing constructors and basic functions like getCurrentLocaiton

--- adventuremain.cpp
+++ adventuremain.cpp
@@ -13,11 +13,14 @@ int main() {
 	//Initializations
 	/*Room r("Test",NULL,NULL,NULL);
 	Room* startroom = &r;*/
-	Player p(100,10,/*startroom*/NULL,NULL); // This is placeholder code.
-
+	Monster mouse1(999,500,"Rowdy Mouse"); //create an enemy
+	Room mouseRoom("dusty room with mice", NULL, NULL, NULL); //create mouse room
+	Room* startingpoint = &mouseRoom; //point to mouse room
+	Player p(100,10,startingpoint,NULL);
 	// Game loop
 	// Flow: Ask for user input, player does action, if monsters are present, they attack, repeat.
 	while (p.isAlive()) { // Continue looping while the player is alive
+		cout << "You are in a " << p.getCurrentLocation() << "." << endl;
 		cout << "Input Command:";
 		getline(cin,input);
 		p.doInput(input); // This function will convert user input into a function call. All game output will be from a function called from here.

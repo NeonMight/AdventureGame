da6dac53aaa8f855a6a46ed135b10760e8b7fa14
justinmc995 <justin.mc995@gmail.com>
2014-11-19 22:33:03 -0600

    Enemies now fight back and can be killed

--- adventuremain.cpp
+++ adventuremain.cpp
@@ -9,23 +9,27 @@ using namespace std;
 
 int main() {
 	string input;
+	bool attackturn = false;
 	
 	//Initializations
 	Item t1("Test Item 1",0,0);
 	Item t2("Test Food",1,10);
 	Item t3("Test Weapon",2,5);
+	Item t4("The Holy Grail",1,10000); // When we start to make enemies fight back, we'll want this for testing
+	Item t5("Test Armor",3,0.5);
 	
-	Monster m1(20,3,"Test Monster");
+	Monster m1(50,3,"Test Monster 1");
+	Monster m2(40,2,"Test Monster 2");
 	
-	Item* r1i[] = {&t1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
-	Monster* r1m[] = {NULL,&m1,NULL,NULL,NULL};
+	Item* r1i[] = {&t1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,&t5,NULL};
+	Monster* r1m[] = {NULL,NULL,NULL,NULL,NULL};
 	Room r1("Test 1",r1i,r1m);
 	
 	Item* r2i[] = {NULL,&t2,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
-	Monster* r2m[] = {NULL,NULL,NULL,NULL,NULL};
+	Monster* r2m[] = {NULL,&m2,NULL,&m1,NULL};
 	Room r2("Test 2",r2i,r2m);
 	
-	Item* r3i[] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
+	Item* r3i[] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,&t4,NULL};
 	Monster* r3m[] = {NULL,NULL,NULL,NULL,NULL};
 	Room r3("Test 3",r3i,r3m);
 	
@@ -50,7 +54,8 @@ int main() {
 	while (p.isAlive()) { // Continue looping while the player is alive
 		cout << "Input Command:";
 		getline(cin,input);
-		p.doInput(input); // This function will convert user input into a function call. All game output will be from a function called from here.
+		attackturn = p.doInput(input); // This function will convert user input into a function call. All game output will be from a function called from here.
+		if (attackturn == true) {p.battle();}
 		cout << "\n";
 	}
 	cout << "You died!\n"; // Game over
